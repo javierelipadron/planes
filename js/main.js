624b24edcd6e3f809e7549592c9d71c5a7535b35
Javier Padron <javierelipadron@gmail.com>
2017-07-25 18:01:14 -0400

    Añadir informacion html, organizacion del css,  js de slider y de tabs

--- js/main.js
+++ js/main.js
@@ -2,6 +2,22 @@
 $(document).ready(function(){
 
 
+    //*********************************************************************/
+    // RUN JS
+    //********************************************************************/
+
+    init();
+
+
+
+
+
+
+
+    /********************************************************************************/
+    /* CREACION Y CLONACION DE CAROUSEL PARA QUE QUEDEN 3 ITEMS ACTIVOS EN PANTALLA.
+    /********************************************************************************/
+    
     $('#myCarousel').carousel({
       interval: false
     })
@@ -24,20 +40,66 @@ $(document).ready(function(){
 
 
 
+
+    /*************************************/
+    /* FUNCIONABILIDAD DE STRECHERS
+    /*************************************/
+
     $(".strecher-blue").click(function() {
+
       if(!$(this).find(".btn-strecher-blue").hasClass("activo")){
+
         $(this).find(".btn-strecher-blue").addClass("activo");
         $(this).find(".btn-strecher-blue").attr("src","img/strecher-menos.png");
+      
       }else{
+
         $(this).find(".btn-strecher-blue").removeClass("activo");
         $(this).find(".btn-strecher-blue").attr("src","img/strecher-mas.png");
+
       }
 
     })
 
-  
 
 
+    /*************************************/
+    /* FUNCIONABILIDAD DE TABS
+    /*************************************/
+    
 
+    $('.tab').click(function(e){
+
+      $('.tab').removeClass("active");
+      let id = $(this).attr("id");
+      $('#' + id).addClass("active");
+
+      let onlyid = id.split("-");
+      show_tabs(onlyid[1]);
+     
+    });
+
+
+    /*************************************/
+    /* INICIALIZACION
+    /*************************************/
+    
+    function init(){
+        
+        show_tabs(1);
+
+    }
+
+
+    /*************************************/
+    /* INICIALIZACION
+    /*************************************/
+
+    function show_tabs(number){
+          
+        $(".content-tab").hide();
+        $("#content-tab-" + number ).show();
+
+    }
 
 })
